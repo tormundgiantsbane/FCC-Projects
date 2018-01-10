7eb612ed78a70197a1a1863ec3487fbf74e6c3f5
Roman Fedoruk <rfedoruk21@gmail.com>
2018-01-10 18:29:21 -0500

    added final touches.

--- Random-Quotes/JS/main.js
+++ Random-Quotes/JS/main.js
@@ -1,4 +1,4 @@
-$("#btnForQuote").click(function(){
+$("#btnForQuote").click(function(e){
 //	$.getJSON('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=', function(result){
 //		 $("#quote").text(result[0].content);
 //		 $("#author").text("- " + result[0].title);
@@ -10,10 +10,30 @@ $("#btnForQuote").click(function(){
 		cache: false,
 		success: function(result){
 			
-		 $("#quote").html(result[0].content);
-		 $("#author").html("- " + result[0].title);
+//		 $("#quote").html(result[0].content);
+//			$("#quote").attr('data-text', result[0].content);
+		 $("#quote").data("text", result[0].content);
+		e.stopPropagation();
+  
+         var text = $('#quote').data('text');
+         	typeWriter(text, 0);
+			$("#author").html("- " + result[0].title);
 		},
 		contentType: "application/x-www-form-urlencoded;charset=utf-8"
 	});
+	
+
+	
 });
 
+
+function typeWriter(text, n) {
+  if (n < (text.length)) {
+    $('#quote').html(text.substring(0, n+1));
+    n++;
+    setTimeout(function() {
+      typeWriter(text, n)
+    }, 50);
+  }
+
+}
